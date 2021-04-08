098fb510a8efee13205838b19507ef9eba45344c
user.email <magdalenasanita@gmail.com>
2021-04-08 15:35:24 +0700

    initial commit

---
+++ twosum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ var twoSum = function(nums, target) {
+    for (let i = 0; i<nums.length; i++){
+        const sum = target - nums[i]
+        for (let j = i+1; j<nums.length; j++){
+            if(sum == nums[j]){
+                return [i, j];   
+            }
+        }
+    }
+};
